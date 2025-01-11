3412de117c187775bac4b0d5a4bed251eaf011e5
Stefan Oltmann <github@stefan-oltmann.de>
2025-01-11 19:59:39 +0100

    Updated KeyGenerator.kt

--- src/test/kotlin/KeyGenerator.kt
+++ src/test/kotlin/KeyGenerator.kt
@@ -1,9 +1,33 @@
-import io.ktor.util.hex
-import kotlin.random.Random
+import java.security.SecureRandom
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import kotlin.experimental.and
+
+fun generateSigningKey(): ByteArray {
+    val secureRandom = SecureRandom()
+    val signingKey = ByteArray(32)
+    secureRandom.nextBytes(signingKey)
+    return signingKey
+}
+
+fun generateEncryptionKey(): SecretKey {
+    val keyGenerator = KeyGenerator.getInstance("AES")
+    keyGenerator.init(256)
+    return keyGenerator.generateKey()
+}
+
+fun ByteArray.toHex(): String {
+    return joinToString("") { "%02x".format(it and 0xFF.toByte()) }
+}
 
 fun main() {
 
-    repeat(2) {
-        println(hex(ByteArray(32).also { Random.Default.nextBytes(it) }))
-    }
+    val signingKey = generateSigningKey()
+    val encryptionKey = generateEncryptionKey()
+
+    val signingKeyHex = signingKey.toHex()
+    val encryptionKeyHex = encryptionKey.encoded.toHex()
+
+    println("Signing Key (Hex): $signingKeyHex")
+    println("Encryption Key (Hex): $encryptionKeyHex")
 }
