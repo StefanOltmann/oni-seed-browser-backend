cf5c20c6fbab8f0bd09e45e09f8e64f80a201d81
Stefan Oltmann <github@stefan-oltmann.de>
2025-04-11 18:07:35 +0200

    Updated KeyGenerator.kt

--- src/test/kotlin/KeyGenerator.kt
+++ src/test/kotlin/KeyGenerator.kt
@@ -1,33 +1,24 @@
-import java.security.SecureRandom
-import javax.crypto.KeyGenerator
-import javax.crypto.SecretKey
-import kotlin.experimental.and
-
-fun generateSigningKey(): ByteArray {
-    val secureRandom = SecureRandom()
-    val signingKey = ByteArray(32)
-    secureRandom.nextBytes(signingKey)
-    return signingKey
-}
-
-fun generateEncryptionKey(): SecretKey {
-    val keyGenerator = KeyGenerator.getInstance("AES")
-    keyGenerator.init(256)
-    return keyGenerator.generateKey()
-}
-
-fun ByteArray.toHex(): String {
-    return joinToString("") { "%02x".format(it and 0xFF.toByte()) }
-}
+import java.security.KeyPairGenerator
+import java.util.Base64
 
 fun main() {
 
-    val signingKey = generateSigningKey()
-    val encryptionKey = generateEncryptionKey()
+    val keyPair = KeyPairGenerator.getInstance("RSA").run {
+        initialize(2048)
+        generateKeyPair()
+    }
 
-    val signingKeyHex = signingKey.toHex()
-    val encryptionKeyHex = encryptionKey.encoded.toHex()
+    println(
+        """
+        # MNI_JWT_PRIVATE_KEY
+        ${Base64.getEncoder().encodeToString(keyPair.private.encoded)}
+        """.trimIndent()
+    )
 
-    println("Signing Key (Hex): $signingKeyHex")
-    println("Encryption Key (Hex): $encryptionKeyHex")
+    println(
+        """
+        # MNI_JWT_PUBLIC_KEY
+        ${Base64.getEncoder().encodeToString(keyPair.public.encoded)}
+        """.trimIndent()
+    )
 }
