3b790fdcd6f4f832421a34da06227a4d0936fec1
Stefan Oltmann <github@stefan-oltmann.de>
2025-07-04 19:47:07 +0200

    Turned option on: Populate search index on start

--- src/main/kotlin/Routings.kt
+++ src/main/kotlin/Routings.kt
@@ -114,7 +114,7 @@ import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 
 /* Should not be necessary right now; was for migration. */
-const val POPULATE_SUMMARIES_ON_START = false
+const val POPULATE_SUMMARIES_ON_START = true
 
 /* Limit the results to avoid memory issues */
 const val RESULT_LIMIT = 100
