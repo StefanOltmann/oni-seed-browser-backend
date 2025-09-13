4457971028c57bdb9034b99b78ce5daa2a92601b
Stefan Oltmann <github@stefan-oltmann.de>
2025-09-13 22:14:46 +0200

    Revert "Use maven central snapshot repo"
    This reverts commit cb9645813c6358aa2f37267fccbbf5c9399065cb.

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,13 +57,23 @@ sourceSets {
 }
 
 repositories {
+
     mavenCentral()
-    maven(url = "https://central.sonatype.com/repository/maven-snapshots/")
+
+    maven {
+
+        url = uri("https://maven.pkg.github.com/StefanOltmann/oni-seed-browser-model")
+
+        credentials {
+            username = System.getenv("GPR_USER")
+            password = System.getenv("GPR_TOKEN")
+        }
+    }
 }
 
 dependencies {
 
-    implementation("de.stefan-oltmann:oni-seed-browser-model:d35090d-SNAPSHOT")
+    implementation("de.stefan-oltmann.oni:model:b719e5e")
 
     /*
      * Ktor server
