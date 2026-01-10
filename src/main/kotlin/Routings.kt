e24f11767b6c9f88192e3bb938baa4074d5fa4d4
Stefan Oltmann <github@stefan-oltmann.de>
2026-01-10 18:28:41 +0100

    Restore backup routine

--- src/main/kotlin/Routings.kt
+++ src/main/kotlin/Routings.kt
@@ -94,6 +94,8 @@ import java.io.File
 import java.security.KeyFactory
 import java.security.interfaces.ECPublicKey
 import java.security.spec.X509EncodedKeySpec
+import java.sql.DriverManager
+import java.util.zip.GZIPOutputStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 import kotlin.io.encoding.Base64
@@ -269,105 +271,83 @@ private fun Application.configureRoutingInternal() {
             call.respondText("util.Benchmark started.")
         }
 
-//        get("/createbackup") {
-//
-//            val apiKey: String? = this.call.request.headers["API_KEY"]
-//
-//            if (apiKey != System.getenv(MNI_DATABASE_EXPORT_API_KEY)) {
-//                call.respond(HttpStatusCode.Unauthorized, "Wrong API key.")
-//                return@get
-//            }
-//
-//            if (currentBackupJob != null) {
-//                call.respond(HttpStatusCode.BadRequest, "Backup job is already running")
-//                return@get
-//            }
-//
-//            currentBackupJob = launch {
-//
-//                log("[BACKUP] Backup creation triggered...")
-//
-//                val backupFile = File(dataDir, "oni-backup.db")
-//                val backupFileZipped = File(dataDir, "oni-backup.db.gz")
-//
-//                backupFile.delete()
-//                backupFileZipped.delete()
-//
-//                try {
-//
-//                    val creationDuration = measureTime {
-//
-//                        val dbFile = File(dataDir, "oni-data.db")
-//
-//                        val srcUrl = "jdbc:sqlite:${dbFile.absolutePath}"
-//
-//                        DriverManager.getConnection(srcUrl).use { conn ->
-//
-//                            /* Ensure no transaction */
-//                            conn.autoCommit = true
-//
-//                            conn.createStatement().use { st ->
-//
-//                                /* Optional: wait a bit if the DB is busy */
-//                                st.execute("PRAGMA busy_timeout = 5000")
-//
-//                                /* Important on Windows: escape backslashes in SQL literal */
-//                                val dstPath = backupFile.absolutePath.replace("\\", "\\\\")
-//
-//                                st.execute("BACKUP TO '$dstPath'")
-//                            }
-//                        }
-//                    }
-//
-//                    log("[BACKUP] Database export took $creationDuration. File size: ${backupFile.length() / 1024 / 1024} MB")
-//
-//                    val compressionDuration = measureTime {
-//
-//                        backupFile.inputStream().use { inputStream ->
-//
-//                            GZIPOutputStream(backupFileZipped.outputStream(), true).use { gzipStream ->
-//                                inputStream.copyTo(gzipStream)
-//                            }
-//                        }
-//                    }
-//
-//                    log("[BACKUP] Compression took $compressionDuration. File size: ${backupFileZipped.length() / 1024 / 1024} MB")
-//
-//                    val uploadDuration = measureTime {
-//
-//                        minioClient.putObject(
-//                            PutObjectArgs
-//                                .builder()
-//                                .bucket(S3_BACKUP_BUCKET)
-//                                .`object`(backupFileZipped.name)
-//                                .headers(
-//                                    mapOf(
-//                                        "Content-Type" to " application/x-sqlite3",
-//                                        "Content-Encoding" to "gzip",
-//                                        /* Cache for 10 years; we manually purge caches. */
-//                                        "Cache-Control" to "public, max-age=315360000, immutable"
-//                                    )
-//                                )
-//                                .stream(backupFileZipped.inputStream(), backupFileZipped.length(), PART_SIZE)
-//                                .build()
-//                        )
-//                    }
-//
-//                    log("[BACKUP] Uploading to S3 took $uploadDuration.")
-//
-//                } catch (ex: Exception) {
-//
-//                    log("[BACKUP] Error on DB export: ${ex.message}")
-//                    log(ex)
-//
-//                } finally {
-//
-//                    currentBackupJob = null
-//                }
-//            }
-//
-//            call.respond(HttpStatusCode.OK, "Backup job started.")
-//        }
+        get("/createbackup") {
+
+            val apiKey: String? = this.call.request.headers["API_KEY"]
+
+            if (apiKey != System.getenv(MNI_DATABASE_EXPORT_API_KEY)) {
+                call.respond(HttpStatusCode.Unauthorized, "Wrong API key.")
+                return@get
+            }
+
+            if (currentBackupJob != null) {
+                call.respond(HttpStatusCode.BadRequest, "Backup job is already running")
+                return@get
+            }
+
+            currentBackupJob = launch {
+
+                log("[BACKUP] Backup creation triggered...")
+
+                val backupFile = File(dataDir, "oni-backup.db")
+                val backupFileZipped = File(dataDir, "oni-backup.db.gz")
+
+                backupFile.delete()
+                backupFileZipped.delete()
+
+                try {
+
+                    val creationDuration = measureTime {
+
+                        val dbFile = File(dataDir, "oni-data.db")
+
+                        val srcUrl = "jdbc:sqlite:${dbFile.absolutePath}"
+
+                        DriverManager.getConnection(srcUrl).use { conn ->
+
+                            /* Ensure no transaction */
+                            conn.autoCommit = true
+
+                            conn.createStatement().use { st ->
+
+                                /* Optional: wait a bit if the DB is busy */
+                                st.execute("PRAGMA busy_timeout = 5000")
+
+                                /* Important on Windows: escape backslashes in SQL literal */
+                                val dstPath = backupFile.absolutePath.replace("\\", "\\\\")
+
+                                st.execute("BACKUP TO '$dstPath'")
+                            }
+                        }
+                    }
+
+                    log("[BACKUP] Database export took $creationDuration. File size: ${backupFile.length() / 1024 / 1024} MB")
+
+                    val compressionDuration = measureTime {
+
+                        backupFile.inputStream().use { inputStream ->
+
+                            GZIPOutputStream(backupFileZipped.outputStream(), true).use { gzipStream ->
+                                inputStream.copyTo(gzipStream)
+                            }
+                        }
+                    }
+
+                    log("[BACKUP] Compression took $compressionDuration. File size: ${backupFileZipped.length() / 1024 / 1024} MB")
+
+                } catch (ex: Exception) {
+
+                    log("[BACKUP] Error on DB export: ${ex.message}")
+                    log(ex)
+
+                } finally {
+
+                    currentBackupJob = null
+                }
+            }
+
+            call.respond(HttpStatusCode.OK, "Backup job started.")
+        }
 
         get("/downloadbackup") {
 
